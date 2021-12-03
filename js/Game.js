60c93736893d337f9d15fc6d1744b49e33116ab7
Eddy Gutierrez <eddytiger17@yahoo.com>
2021-12-03 18:28:34 -0500

    update js files

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,41 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    
+    constructor(){
+       this.missed = 0;
+       this.phrases = [
+        new Phrase("Hello World"),
+        new Phrase("I love programming"),
+        new Phrase("Stress is fun"),
+        new Phrase("Keep Pushing"),
+        new Phrase("Good Luck")];
+       this.activePhrase = null; 
+    }
+
+   
+
+
+    startGame(){
+       document.getElementById('overlay').style.display = 'none';
+       this.getRandomPhrase().addPhraseToDisplay();
+    }
+
+
+    getRandomPhrase(){
+        const randomPhrase = new Phrase(this.phrases[Math.floor(Math.random() * this.phrases.length)].phrase);
+        //new Phrase(this.phrases[Math.floor(Math.random() * this.phrases.length)]) ;
+        return randomPhrase;
+            //return a random phrase from the array of objects 
+    }
+
+    handleInteraction(){
+
+    }
+}
+
+
+
+
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,45 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+
+class Phrase{
+    
+    constructor(phrase){
+         this.phrase = phrase.toLowerCase();
+    }
+    
+    
+
+    
+    addPhraseToDisplay(){
+
+
+    const displayDiv = document.querySelector('#phrase ul');
+    //console.log(displayDiv);
+    const splitString = this.phrase.split(''); 
+    splitString.forEach(element => {
+       const liElem = document.createElement('li');
+       
+       if(element !== " "){
+          liElem.setAttribute('class', "hide letter " + element);
+          liElem.textContent = element;
+       }else{
+          liElem.setAttribute('class','space'); 
+          liElem.textContent = " ";
+       }
+       
+        displayDiv.append(liElem);
+            
+    });
+    }
+
+    checkLetter(){
+
+    }
+
+    showMatchedLetter(){
+
+    }
+}
+
